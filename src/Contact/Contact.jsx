86137571d02fa3970089e224a366b0569fa5f881
olena-savchenko <112640422+olena-savchenko@users.noreply.github.com>
2024-05-13 16:25:12 +0300

    add css component

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,7 +1,17 @@
+import css from './Contact.module.css'
+import { ImPhone } from "react-icons/im";
+import { ImUser } from "react-icons/im";
+
 export default function Contact ({contact: { name, number}}) {
     return (
-        <li><p>Name: {name}</p>
-        <p>Number: {number}</p></li>
+        <li className={css.listItem}>
+            <div>
+            <p className={css.text}><ImUser /> {name}</p>
+            <p className={css.text}><ImPhone /> {number}</p>
+            </div>
+
+            <button className={css.btn}>Delete</button>    
+        </li>
         
     );
 }
--- src/ContactList/ContactList.jsx
+++ src/ContactList/ContactList.jsx
@@ -1,7 +1,8 @@
 import Contact from "../Contact/Contact";
+import css from './ContactList.module.css';
 
 export default function ContactList ({contacts}) {
-   return (<ul>{
+   return (<ul className={css.list}>{
         contacts.map(contact => {
             return <Contact key={contact.id} contact={contact}/>
         })
